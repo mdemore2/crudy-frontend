46fc55ff134c1fade030b23482646837d39faf69
Mark Demore II <53943861+mdemore2@users.noreply.github.com>
2026-01-18 20:13:10 -0500

    basic styling

---
+++ wearhaus/src/components/home.jsx
@@ -0,0 +1,15 @@
+import ItemCard from "./itemcard"
+import "../styles/home.css"
+
+function Home({items}){
+
+    return <div className="home">
+    
+        {items.map((item) => {
+            return <ItemCard key={item['id']} item={item}/>
+        })}
+
+    </div>
+}
+
+export default Home;
--- wearhaus/src/components/itemcard.jsx
+++ wearhaus/src/components/itemcard.jsx
@@ -1,9 +1,10 @@
+import '../styles/itemcard.css';
 function ItemCard({item}){
     //TODO: implement :)
     return <div className="card">
         <h5>{item.name}</h5>
         <p>{item.description}</p>
-        <h5>{item.quantity}</h5>
+        <h5>Quantity: {item.quantity}</h5>
     </div>
 }
 
